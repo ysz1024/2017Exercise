e9ec179c0405ac0f6500bf6511079e9ec858a9f3
袁思曾 <yuansizeng@192.168.0.103>
2017-04-27 22:20:20 +0800

    commit

--- 2017Exercise/WaterCollectionViewLayout.swift
+++ 2017Exercise/WaterCollectionViewLayout.swift
@@ -14,5 +14,30 @@ class WaterCollectionViewLayout: UICollectionViewLayout {
     var setSize:()->(Array<UIImage>) = {_ in return []}
     var queueNum:Int = 2
     var hs:Array<CGFloat>!
+    private var totalNum:Int!
+    private var layoutAttributes:Array<UICollectionViewLayoutAttributes>!
+    
+    override func prepare() {
+        super.prepare()
+        hs = []
+        for _ in 0..<queueNum{
+            hs.append(5)
+        }
+        totalNum = collectionView?.numberOfItems(inSection: 0)
+        layoutAttributes = []
+        var indexpath:NSIndexPath!
+        for index in 0..<totalNum{
+            indexpath = NSIndexPath(row: index, section: 0)
+            let attributes = layoutAttributesForItem(at: indexpath as IndexPath)
+            layoutAttributes.append(attributes!)
+        }
+    }
+    
+    private let gap:CGFloat = 5//间隔
+    private var width:CGFloat!
+    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        width = collectionView?.bounds.size.width - gap*
+        
+    }
 
 }
