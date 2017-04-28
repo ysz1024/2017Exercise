25226640473175ba13a60cd7db56569bb129501e
ysz1024 <ysz1024@qq.com>
2017-04-28 16:41:52 +0800

    commit

--- 2017Exercise/WaterCollectionViewLayout.swift
+++ 2017Exercise/WaterCollectionViewLayout.swift
@@ -36,8 +36,52 @@ class WaterCollectionViewLayout: UICollectionViewLayout {
     private let gap:CGFloat = 5//间隔
     private var width:CGFloat!
     override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
-        width = collectionView?.bounds.size.width - gap*
+        width = ((collectionView?.bounds.size.width)! - gap*(CGFloat(queueNum)+1))/CGFloat(queueNum)
+        let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
+        let sizes = setSize()
         
+        attributes.size = CGSize(width: width, height: sizes[indexPath.row].size.height*width/sizes[indexPath.row].size.width)
+        var nub:CGFloat = 0
+        var h:CGFloat = 0
+        (nub,h)= minH(hhs: hs)
+        attributes.center = CGPoint(x: (nub+0.5)*(gap+width), y: <#T##CGFloat#>)
+    
+        
+    }
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        return layoutAttributes
+    }
+    
+    override var collectionViewContentSize: CGSize{
+        get {
+            return CGSize(width: (width:(collectionView?.bounds.width)!), height: maxH(hhs: hs))
+        }
+        set {
+            self.collectionViewContentSize = newValue
+        }
+    }
+    
+    private func minH(hhs:Array<CGFloat>)->(CGFloat,CGFloat){
+        var num = 0
+        var min = hhs[0]
+        for i in 1..<hhs.count{
+            if min>hhs[i]{
+                min = hhs[i]
+                num = i
+            }
+        }
+        return(CGFloat(num),min)
+    }
+    
+    func maxH(hhs:Array<CGFloat>)->CGFloat{
+        var max = hhs[0]
+        for i in 1..<hhs.count{
+            if max < hhs[i]{
+                max = hhs[i]
+            }
+            
+        }
+        return max
     }
 
 }
