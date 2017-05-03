fd086dabfbb6708b53966ffe24e116f7b23f6dfb
袁思曾 <yuansizeng@192.168.0.100>
2017-05-03 21:10:36 +0800

    commit

--- MapViewController.swift
+++ MapViewController.swift
@@ -13,6 +13,7 @@ import MapKit
 class MapViewController: UIViewController {
 
     @IBOutlet var customMapView: MKMapView!
+    //定位管理器
     private lazy var location:CLLocationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,17 +21,25 @@ class MapViewController: UIViewController {
         self.customMapView.showsPointsOfInterest = true
         self.customMapView.showsUserLocation = true
         self.customMapView.userTrackingMode = .followWithHeading
+        //地图类型
         if #available(iOS 9.0, *) {
             self.customMapView.mapType = .hybridFlyover
         }
         self.customMapView.mapType = .standard
-        self.customMapView.showsCompass = true
-        self.customMapView.showsBuildings = true
-        self.customMapView.showsTraffic = true
         
-        self.customMapView.isZoomEnabled = true
-        self.customMapView.isRotateEnabled = true
-        self.customMapView.isScrollEnabled = true
+        //创建MKCoordinateSpan对象，设置地图范围（越小越精确）
+        let latDelta = 0.05
+        let longDelta = 0.05
+        let currentLocationSpan:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        
+        
+//        self.customMapView.showsCompass = true
+//        self.customMapView.showsBuildings = true
+//        self.customMapView.showsTraffic = true
+//        
+//        self.customMapView.isZoomEnabled = true
+//        self.customMapView.isRotateEnabled = true
+//        self.customMapView.isScrollEnabled = true
         
         // Do any additional setup after loading the view.
     }
@@ -42,21 +51,21 @@ class MapViewController: UIViewController {
     }
     
 }
-    extension MapViewController: MKMapViewDelegate {
-        //当地图获取到用户的位置的时候回调用该方法
-        func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
-            userLocation.title = ""
-            userLocation.subtitle = ""
-            let center = userLocation.location?.coordinate
-            customMapView.setCenter(center!, animated: true)
-            
-            //改变显示区域
-            let span = MKCoordinateSpanMake(2.2, 48.52)
-            let region = MKCoordinateRegionMake(center!, span)
-            mapView.setRegion(region, animated: true)
-            
-        }
-    }
+//    extension MapViewController: MKMapViewDelegate {
+//        //当地图获取到用户的位置的时候回调用该方法
+//        func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+//            userLocation.title = ""
+//            userLocation.subtitle = ""
+//            let center = userLocation.location?.coordinate
+//            customMapView.setCenter(center!, animated: true)
+//            
+//            //改变显示区域
+//            let span = MKCoordinateSpanMake(2.2, 48.52)
+//            let region = MKCoordinateRegionMake(center!, span)
+//            mapView.setRegion(region, animated: true)
+//            
+//        }
+//    }
     
 
     
